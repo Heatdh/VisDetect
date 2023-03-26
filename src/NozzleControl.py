677bac3a764d605af4906d7bd8a40be0640af970
Thang Tran <trankimthang279@gmail.com>
2023-03-26 12:06:31 +0200

    added skeleton for APIs

---
+++ src/NozzleControl.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class SprayPosition(Enum):
+    Left = 0,
+    Right = 1
+
+class NozzleControl:
+    def __init__(self):
+        pass
+
+    def spray(sprayPosition : SprayPosition):
+        pass
