91c5bfae53281ea7c5ed119ee61ac3415958332a
Nhat Cuong Ha <nhatcuong94@gmail.com>
2023-03-26 16:48:50 +0200

    Stepper motor controller draft Jetson robot movement draft

---
+++ src/controller/jetson_robot_movement.py
@@ -0,0 +1,43 @@
+from controller.robot_movement import RobotMovement
+from controller.stepper_motor import StepperMotor
+
+class JetsonRobotMovement(RobotMovement):
+    """ Controls the movement of the robot using the Nvidia Jetson Nano GPIO pins.
+
+    Args:
+        RobotMovement (_type_): _description_
+    """
+    def __init__(self, left_motor_pins, right_motor_pins):
+        self.left_motor = StepperMotor(*left_motor_pins)
+        self.right_motor = StepperMotor(*right_motor_pins)
+
+    def move_forward(self, distance):
+        # Calculate the number of steps to move forward the specified distance
+        # (assuming a wheel diameter of 10cm and a step angle of 1.8 degrees)
+        steps = int(distance / (3.1416 * 10 / 200) / 1.8)
+
+        # Move both wheels forward by the specified number of steps
+        self.left_motor.move_steps(steps, 'CW')
+        self.right_motor.move_steps(steps, 'CW')
+
+    def step_left(self, angle):
+        # Calculate the number of steps to turn left by the specified angle
+        # (assuming a wheelbase of 20cm and a step angle of 1.8 degrees)
+        steps = int((3.1416 * 20 / 360) * angle / (3.1416 * 10 / 200) / 1.8)
+
+        # Move the left wheel backward and the right wheel forward by the specified number of steps
+        self.left_motor.move_steps(steps, 'CCW')
+        self.right_motor.move_steps(steps, 'CW')
+
+    def step_right(self, angle):
+        # Calculate the number of steps to turn right by the specified angle
+        # (assuming a wheelbase of 20cm and a step angle of 1.8 degrees)
+        steps = int((3.1416 * 20 / 360) * angle / (3.1416 * 10 / 200) / 1.8)
+
+        # Move the right wheel backward and the left wheel forward by the specified number of steps
+        self.right_motor.move_steps(steps, 'CCW')
+        self.left_motor.move_steps(steps, 'CW')
+        
+    def stop(self):
+        self.left_motor.stop()
+        self.right_motor.stop()
