4e9c5add03e74c1bf20bcf213f5af09ebf63a00f
Heatdh <rayendhahri@gmail.com>
2023-03-28 11:53:07 +0200

    functional detect/ color based decision

--- src/vision/detection.py
+++ src/vision/detection.py
@@ -1,5 +1,8 @@
 import cv2 as cv
 import numpy as np
+class VisDetector():
+    def __init__(self) -> None:
+        pass
 
 def detect_circles_blob(frame):
     params = cv.SimpleBlobDetector_Params()
@@ -43,6 +46,24 @@ def detect_circles_blob(frame):
 
     return circle_data
 
+def label_green_circles(frame, circle_data, hue_range=(30, 90), min_saturation=80, min_value=80):
+    green_circle_coordinates = []
+
+    hsv_frame = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+
+    for keypoint, _ in circle_data:
+        x, y = int(keypoint.pt[0]), int(keypoint.pt[1])
+        hsv_color = hsv_frame[y, x]
+
+        if hue_range[0] <= hsv_color[0] <= hue_range[1] and hsv_color[1] >= min_saturation and hsv_color[2] >= min_value:
+            radius = int(keypoint.size / 2)
+            cv.circle(frame, (x, y), radius, (0, 255, 0), 2)
+            green_circle_coordinates.append((x, y, radius))
+            
+    return green_circle_coordinates
+
+
+
 
 def intersection_with_horizontal_line(circle_data, h):
     intersection_points = []
@@ -56,6 +77,21 @@ def intersection_with_horizontal_line(circle_data, h):
             intersection_points.append((x + dx, h))
 
     return intersection_points
+
+def intersection_green_horizontal(circle_data, h):
+    print(circle_data)
+    intersection_points = []
+    for keypoint in circle_data:
+        x, y = int(keypoint[0]), int(keypoint[1])
+        radius = keypoint[2]
+        
+        if abs(h - y) <= radius:
+            dx = np.sqrt(radius ** 2 - (h - y) ** 2)
+            intersection_points.append((x - dx, h))
+            intersection_points.append((x + dx, h))
+
+    return intersection_points
+   
 
 
 if __name__ == "__main__":
@@ -76,25 +112,49 @@ if __name__ == "__main__":
                                                 cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
         for keypoint, avg_color in circle_data:
-            print(f"Circle at ({int(keypoint.pt[0])}, {int(keypoint.pt[1])}) has color {avg_color}")
+            #print(f"Circle at ({int(keypoint.pt[0])}, {int(keypoint.pt[1])}) has color {avg_color}")
+            pass
 
         # Define the horizontal line's height (e.g., half of the frame height)
         h = frame.shape[0] // 2
 
         # Draw the horizontal line
         cv.line(frame_with_keypoints, (0, h), (frame.shape[1], h), (0, 255, 0), 2)
+        # draw vertical line 
+        cv.line(frame_with_keypoints, (frame.shape[1]//2, 0), (frame.shape[1]//2, frame.shape[0]), (0, 255, 0), 2)
 
         # Find the intersection points between the circles and the horizontal line
         intersection_points = intersection_with_horizontal_line(circle_data, h)
 
-        # Draw the intersection points
-        for point in intersection_points:
-            cv.circle(frame_with_keypoints, (int(point[0]), int(point[1])), 3, (255, 0, 0), -1)
+        green_circle_coordinates = label_green_circles(frame_with_keypoints, circle_data)
 
+        # Print the coordinates of the green circles
+        for coord in green_circle_coordinates:
+            print(f"Green circle at {coord}")
+        print(f"in each frame there are {len(green_circle_coordinates)} green circles")
+
+        if len(green_circle_coordinates) > 0:
+            green_circle_intersections = intersection_green_horizontal(green_circle_coordinates, h)
+            print(f"green circle intersections {green_circle_intersections}")
+            if len(green_circle_intersections)> 0:
+                for point in green_circle_intersections:
+                    cv.circle(frame_with_keypoints, (int(point[0]), int(point[1])), 3, (255, 0, 0), -1)
+
+        # Draw the intersection points
+        #if green_circle_intersections is not None:
+        #    for point in green_circle_intersections:
+        #        cv.circle(frame_with_keypoints, (int(point[0]), int(point[1])), 3, (255, 0, 0), -1)
+                
+        #for point in intersection_points:
+        #  cv.circle(frame_with_keypoints, (int(point[0]), int(point[1])), 3, (255, 0, 0), -1)
+
+        
+
+        label_green_circles(frame_with_keypoints, circle_data)
         cv.imshow('Robot Controller', frame_with_keypoints)
 
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
-
+        
     cap.release()
     cv.destroyAllWindows()
