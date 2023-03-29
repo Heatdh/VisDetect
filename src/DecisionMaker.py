8f9cd1e51d27526e0441ec4bf4c0ec723a0f75b1
Heatdh <rayendhahri@gmail.com>
2023-03-30 00:57:53 +0200

    pixel distance to green

--- src/DecisionMaker.py
+++ src/DecisionMaker.py
@@ -1,3 +1,4 @@
+import numpy as np
 class DecisionMaker:
     def __init__(self):
         self.move = 0
@@ -12,6 +13,9 @@ class DecisionMaker:
         else:
             self.move = 1
             self.stop = 0
+            green_center = np.mean(green_circle_coordinates, axis=0)
+            frame_center = np.array([len(green_circle_coordinates[0])/2, len(green_circle_coordinates)/2])
+            self.distance_to_green = int(green_center[0] - frame_center[0])
 
         if green_intersection:
             self.stop = 1
