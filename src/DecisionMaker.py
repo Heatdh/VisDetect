f0f2b3539d2a79ded13aa0af04c5cb9783327a4b
Heatdh <rayendhahri@gmail.com>
2023-03-30 09:46:58 +0200

    decisionmaker edit

--- src/DecisionMaker.py
+++ src/DecisionMaker.py
@@ -1,3 +1,4 @@
+import numpy as np
 class DecisionMaker:
     def __init__(self):
         self.move = 0
@@ -12,6 +13,17 @@ class DecisionMaker:
         else:
             self.move = 1
             self.stop = 0
+            closest_circle = None
+            for circle in green_circle_coordinates:
+                if circle[1] >= len(green_circle_coordinates)/2:
+                    if closest_circle is None or abs(circle[0] - len(green_circle_coordinates[0])/2) < abs(closest_circle[0] - len(green_circle_coordinates[0])/2):
+                        closest_circle = circle
+            if closest_circle is not None:
+                # calculate distance to closest green circle
+                frame_center = np.array([len(green_circle_coordinates[0])/2, len(green_circle_coordinates)/2])
+                self.distance_to_green = int(closest_circle[0] - frame_center[0])
+            else:
+                self.distance_to_green = 0
 
         if green_intersection:
             self.stop = 1
